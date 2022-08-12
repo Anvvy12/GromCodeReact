ecf6f566eac775b6ae5b5400bb96e1053c33e90e
Bogdan <youbody7@gmail.com>
2022-08-12 20:40:09 +0300

    s

--- Lesson_3/task_3/src/index.jsx
+++ Lesson_3/task_3/src/index.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Search from "./Search.jsx";
 import "./styles/styles.css";
+import Comment from "./Comments";
 
 const rootElem = document.querySelector("#root");
 
-ReactDOM.render(<Search name="Tom" />, rootElem);
+const normalDate = (date) => {
+  return `${date.getDay()} ${date.toLocaleString("en", {
+    month: "long",
+  })} ${date.getFullYear()}`;
+};
+
+const userInfo = {
+  name: "Tom",
+  avatarUrl:
+    "https://images.sftcdn.net/images/t_app-logo-xl,f_auto/p/31e24716-8dd4-4de2-bd4a-8f888bc42d01/2028187188/path-of-building-logo.png",
+};
+
+ReactDOM.render(
+  <Comment user={userInfo} text="Good jop!" date={normalDate(new Date())} />,
+  rootElem
+);
