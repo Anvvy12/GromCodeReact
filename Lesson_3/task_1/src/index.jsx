ee153ed1c02e1b4d203c284c08b7447f96fe4064
Bogdan <youbody7@gmail.com>
2022-08-12 16:34:36 +0300

    s

--- Lesson_3/task_1/src/index.jsx
+++ Lesson_3/task_1/src/index.jsx
@@ -1,20 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/style.css";
+import Search from "./Search.jsx";
 
 const rootElem = document.querySelector("#root");
 
-const Greeting = () => {
-  const element = (
-    <div class="search">
-      <h1 class="search__title">Hello, Tom. What to search for you?</h1>
-      <div class="search__field">
-        <input type="text" class="search__input" />
-        <button class="search__button">Search</button>
-      </div>
-    </div>
-  );
-  return element;
-};
-
-ReactDOM.render(<Greeting />, rootElem);
+ReactDOM.render(<Search name="Tom" />, rootElem);
