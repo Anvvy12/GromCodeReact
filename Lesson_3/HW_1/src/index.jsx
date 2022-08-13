c0c0b3e481ad88610bdeb870fadcaf501b74198d
Bogdan <youbody7@gmail.com>
2022-08-13 16:45:32 +0300

    s

--- Lesson_3/HW_1/src/index.jsx
+++ Lesson_3/HW_1/src/index.jsx
@@ -7,7 +7,6 @@ const rootElem = document.querySelector("#root");
 const getAge = (dob = new Date(1990, 9, 31)) => {
   const now = new Date();
   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-
   const dobnow = new Date(today.getFullYear(), dob.getMonth(), dob.getDate());
   let age = today.getFullYear() - dob.getFullYear();
   if (today < dobnow) {
@@ -18,6 +17,10 @@ const getAge = (dob = new Date(1990, 9, 31)) => {
 getAge();
 
 ReactDOM.render(
-  <Greeting firstName={"Mark"} lastName={"Nikoda"} birthDate={getAge()} />,
+  <Greeting
+    firstName={"Mark"}
+    lastName={"Nikoda"}
+    birthDate={getAge(new Date(2010, 9, 31))}
+  />,
   rootElem
 );
