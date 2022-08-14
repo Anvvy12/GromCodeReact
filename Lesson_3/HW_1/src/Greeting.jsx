399d6e7877e081af88e2c266f05fb4d222d44f53
Bogdan <youbody7@gmail.com>
2022-08-14 20:26:14 +0300

    s

--- Lesson_3/HW_1/src/Greeting.jsx
+++ Lesson_3/HW_1/src/Greeting.jsx
@@ -3,6 +3,9 @@ import "./index.css";
 
 export default (props) => {
   return (
-    <div className="greeting">{`My name is ${props.firstName} ${props.lastName}. I'm ${props.birthDate} years old`}</div>
+    <div className="greeting">
+      My name is {props.firstName} {props.lastName}. I'm {props.birthDate} years
+      old
+    </div>
   );
 };
