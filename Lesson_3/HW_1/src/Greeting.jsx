2da9bf54190e9602cc7b8eb630f885eca039bf8e
Bogdan <youbody7@gmail.com>
2022-08-14 20:15:22 +0300

    s

--- Lesson_3/HW_1/src/Greeting.jsx
+++ Lesson_3/HW_1/src/Greeting.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./index.scss";
 
-export const Greeting = (props) => {
+export default (props) => {
   return (
     <div className="greeting">{`My name is ${props.firstName} ${props.lastName}. I'm ${props.birthDate} years old`}</div>
   );
