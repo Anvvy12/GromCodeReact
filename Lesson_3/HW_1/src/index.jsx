05933914cbcb8960fa72b8195d3198c13b99f245
Bogdan <youbody7@gmail.com>
2022-08-14 20:10:00 +0300

    s

--- Lesson_3/HW_1/src/index.jsx
+++ Lesson_3/HW_1/src/index.jsx
@@ -11,7 +11,7 @@ ReactDOM.render(
   <Greeting
     firstName={"John"}
     lastName={"Doe"}
-    birthDate={formatDate("2005-07-17")}
+    birthDate={formatDate("2004-09-17")}
     // age={birthDate}
   />,
   rootElem
