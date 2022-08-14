eabd28e46a53eb6f0511d95867accef70e1a54d3
Bogdan <youbody7@gmail.com>
2022-08-14 19:59:05 +0300

    s

--- Lesson_3/HW_1/src/index.jsx
+++ Lesson_3/HW_1/src/index.jsx
@@ -1,26 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Greeting from "./Greeting";
+import moment from "moment";
 
 const rootElem = document.querySelector("#root");
-
-const getAge = (dob = new Date(1990, 9, 31)) => {
-  const now = new Date();
-  const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-  const dobnow = new Date(today.getFullYear(), dob.getMonth(), dob.getDate());
-  let age = today.getFullYear() - dob.getFullYear();
-  if (today < dobnow) {
-    age === 0 ? age : age - 1;
-  }
-  return age;
-};
-getAge();
+const formatDate = (date) => moment().diff(date, "years");
 
 ReactDOM.render(
   <Greeting
     firstName={"Mark"}
     lastName={"Nikoda"}
-    birthDate={getAge(new Date(2010, 9, 31))}
+    birthDate={formatDate()}
     // age={birthDate}
   />,
   rootElem
