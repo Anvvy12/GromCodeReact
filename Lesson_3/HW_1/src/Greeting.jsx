0e04dadac463fbd61d0a871d9266bad2337bfb30
Bogdan <youbody7@gmail.com>
2022-08-14 20:20:46 +0300

    s

--- Lesson_3/HW_1/src/Greeting.jsx
+++ Lesson_3/HW_1/src/Greeting.jsx
@@ -1,25 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import Greeting from "./Greeting";
-import moment from "moment";
 import "./index.scss";
-
-const rootElem = document.querySelector("#root");
-
-const formatDate = (date) => moment().diff(date, "years", false);
 
 export default (props) => {
   return (
     <div className="greeting">{`My name is ${props.firstName} ${props.lastName}. I'm ${props.birthDate} years old`}</div>
   );
 };
-
-ReactDOM.render(
-  <Greeting
-    firstName={"John"}
-    lastName={"Doe"}
-    birthDate={formatDate("2004-09-17")}
-    // age={birthDate}
-  />,
-  rootElem
-);
