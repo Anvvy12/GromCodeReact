61bdea0fc624ebec5fc86e867d970dad9962c3b8
Bogdan <youbody7@gmail.com>
2022-08-14 20:05:41 +0300

    s

--- Lesson_3/HW_1/src/index.jsx
+++ Lesson_3/HW_1/src/index.jsx
@@ -4,13 +4,14 @@ import Greeting from "./Greeting";
 import moment from "moment";
 
 const rootElem = document.querySelector("#root");
-const formatDate = (date) => moment().diff(date, "years");
+
+const formatDate = (date) => moment().diff(date, "years", false);
 
 ReactDOM.render(
   <Greeting
     firstName={"Mark"}
     lastName={"Nikoda"}
-    birthDate={formatDate()}
+    birthDate={formatDate("2022-07-17")}
     // age={birthDate}
   />,
   rootElem
