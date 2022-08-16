94e69f28fb52b309e19b2861436a2ebaef23d7fb
Bogdan <youbody7@gmail.com>
2022-08-16 13:37:07 +0300

    s

--- lesson4/hw1/src/App.jsx
+++ lesson4/hw1/src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <>
       <Clock location="New York" offset={-5} />
-      <Clock location="Kyiv" offset={3} />
+      <Clock location="Kyiv" offset={2} />
       <Clock location="London" offset={0} />
     </>
   );
