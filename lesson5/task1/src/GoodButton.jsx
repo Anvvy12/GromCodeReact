aebb976b6ff4bedfeb65470fb9592a6fadbaef27
Bogdan <youbody7@gmail.com>
2022-08-16 18:09:47 +0300

    s

---
+++ lesson5/task1/src/GoodButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+  return (
+    <button className="fancy-button" onClick={() => alert("Good job!")}>
+      Click on me!
+    </button>
+  );
+};
