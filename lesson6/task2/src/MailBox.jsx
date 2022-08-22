1b64fb9dd792c9ac6ed76ab300d858300af8bb49
Bogdan <youbody7@gmail.com>
2022-08-22 17:44:00 +0300

    s

--- lesson6/task2/src/MailBox.jsx
+++ lesson6/task2/src/MailBox.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default ({ unreadMessage }) => {
+export default ({ unreadMessages }) => {
   return (
     <div className="mailbox">
       <span className="mailbox__text">Messages</span>
       {unreadMessage.length > 0 && (
-        <span className="mailbox__count">{unreadMessage.length}</span>
+        <span className="mailbox__count">{unreadMessages.length}</span>
       )}
     </div>
   );
