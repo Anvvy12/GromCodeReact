05ab865d77c43febad9559558f7d10a818ecdc60
Bogdan <youbody7@gmail.com>
2022-08-22 20:46:05 +0300

    s

--- lesson6/task2/src/index.jsx
+++ lesson6/task2/src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Mailbox from "./Mailbox";
+import Mailbox from "./s";
 import "./styles/index.scss";
 
 const rootElem = document.querySelector("#root");
