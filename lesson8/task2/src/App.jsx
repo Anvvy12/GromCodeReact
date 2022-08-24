fab1e98046360f0b5a75986d44a6a968bf4bbd03
Bogdan <youbody7@gmail.com>
2022-08-24 20:23:49 +0300

    s

--- lesson8/task2/src/App.jsx
+++ lesson8/task2/src/App.jsx
@@ -22,9 +22,9 @@ export default class extends Component {
         <button onClick={this.toggle}>Toggle</button>
         {this.state.visible && (
           <>
-            <Clock location={"New York"} offset={-5} />
-            <Clock location={"Kyiv"} offset={2} />
-            <Clock location={"London"} offset={0} />
+            <Clock location={"New York"} offset={-4} />
+            <Clock location={"Kyiv"} offset={3} />
+            <Clock location={"London"} offset={1} />
           </>
         )}
       </>
