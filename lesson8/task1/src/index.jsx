c250aca45956e5cd79627af70eaa1cff5086ac70
Bogdan <youbody7@gmail.com>
2022-08-24 18:52:42 +0300

    s

--- lesson8/task1/src/index.jsx
+++ lesson8/task1/src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Life from "./Life";
 import "./styles/index.scss";
 
 const rootElem = document.querySelector("#root");
 
-ReactDOM.render(<App />, rootElem);
+ReactDOM.render(<Life />, rootElem);
