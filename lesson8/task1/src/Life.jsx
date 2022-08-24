c4d2daafe76ab7bd10db73dc9ecdaf5b175509ad
Bogdan <youbody7@gmail.com>
2022-08-24 19:00:43 +0300

    s

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -13,6 +13,7 @@ class Life extends Component {
     console.log(
       "shouldComponentUpdate(nextProps, nextState): decide to render or not to render"
     );
+    return true;
   }
   componentWillUnmount() {
     console.log(
@@ -20,11 +21,9 @@ class Life extends Component {
     );
   }
   componentDidUpdate() {
-    if (true) {
-      console.log(
-        "componentDidUpdate(prevProps, prevState): some updates based on new props"
-      );
-    }
+    console.log(
+      "componentDidUpdate(prevProps, prevState): some updates based on new props"
+    );
   }
 
   render() {
