201a52c2e2c6aca624b8b71ac44ac88d762aa900
Bogdan <youbody7@gmail.com>
2022-08-29 18:49:03 +0300

    s

--- lesson8/hw1/src/ConnectionStatus.jsx
+++ lesson8/hw1/src/ConnectionStatus.jsx
@@ -4,7 +4,7 @@ class ConnectionStatus extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isOnline: null,
+      isOnline: true,
     };
   }
   onlineStatus = () => {
