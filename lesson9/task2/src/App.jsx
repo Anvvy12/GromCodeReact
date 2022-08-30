fac26af00a7d25d1de95fe74f5b23eb6e33ac50b
Bogdan <youbody7@gmail.com>
2022-08-30 15:47:34 +0300

    s

--- lesson9/task2/src/App.jsx
+++ lesson9/task2/src/App.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { Component } from "react";
 import UserForm from "./UserForm";
 
-export default () => {
-  return <UserForm />;
-};
+export default class extends Component {
+  createUser = (obj) => {
+    console.log(obj);
+  };
+  render() {
+    return <UserForm onSubmit={this.createUser} />;
+  }
+}
