cccadcff669a9686f477b91ac9aaf0a067151486
Bogdan <youbody7@gmail.com>
2022-08-30 15:41:19 +0300

    s

--- lesson9/task2/src/App.jsx
+++ lesson9/task2/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import GoodButton from "./GoodButton";
+import UserForm from "./UserForm";
 
 export default () => {
-  return <GoodButton />;
+  return <UserForm />;
 };
