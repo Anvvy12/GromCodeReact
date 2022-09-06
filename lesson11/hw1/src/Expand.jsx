1841f85d40e4bd8969a5a8513905b1ce15c98f8c
Bogdan <youbody7@gmail.com>
2022-09-06 21:00:07 +0300

    ы

--- lesson11/hw1/src/Expand.jsx
+++ lesson11/hw1/src/Expand.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faChevronUp, faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 export default ({ children, title, toggler, isFull }) => {
   const arrow = isFull ? (
