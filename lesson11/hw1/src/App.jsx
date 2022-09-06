b88e4f49463abecced56935e365578536bc9be08
Bogdan <youbody7@gmail.com>
2022-09-06 19:38:50 +0300

    s

--- lesson11/hw1/src/App.jsx
+++ lesson11/hw1/src/App.jsx
@@ -1,6 +1,31 @@
-import React from "react";
-import GoodButton from "./GoodButton";
+import React, { Component } from "react";
+import Expand from "./Expand";
 
-export default () => {
-  return <GoodButton />;
-};
+export default class extends Component {
+  state = {
+    isFull: false,
+  };
+
+  toggleInfo = () => {
+    this.setState({
+      isFull: !this.state.isFull,
+    });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <Expand
+          title={"Some title"}
+          toggler={this.toggleInfo}
+          isFull={this.state.isFull}
+        >
+          <p>
+            Hooks are a new addition in React 16.8. They let you use state and
+            other React features without writing a class.
+          </p>
+        </Expand>
+      </div>
+    );
+  }
+}
---
+++ lesson11/hw1/src/Expand.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronUp, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+
+export default ({ children, title, toggler, isFull }) => {
+  const arrow = isFull ? (
+    <FontAwesomeIcon icon={faChevronUp} />
+  ) : (
+    <FontAwesomeIcon icon={faChevronDown} />
+  );
+
+  const info = isFull ? children : null;
+  return (
+    <div className="expand border">
+      <div className="expand__header">
+        <span className="expand__title">{title}</span>
+        <button className="expand__toggle-btn" onClick={toggler}>
+          {arrow}
+        </button>
+      </div>
+      <div className="expand__content">{info}</div>
+    </div>
+  );
+};
