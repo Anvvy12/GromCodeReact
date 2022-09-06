9389c1c83e8f3418b69309b66be9fc5f3bfa2798
Bogdan <youbody7@gmail.com>
2022-09-06 21:15:01 +0300

    s

--- lesson11/hw1/src/Expand.jsx
+++ lesson11/hw1/src/Expand.jsx
@@ -1,22 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default ({ children, title, toggler, isFull }) => {
-  const arrow = isFull ? (
-    <i className="fa fa-chevron-up" aria-hidden="true"></i>
-  ) : (
-    <i className="fa fa-chevron-down" aria-hidden="true"></i>
-  );
+export default class extends Component {
+  state = {
+    isFull: false,
+  };
 
-  const info = isFull ? children : null;
-  return (
-    <div className="expand border">
-      <div className="expand__header">
-        <span className="expand__title">{title}</span>
-        <button className="expand__toggle-btn" onClick={toggler}>
-          {arrow}
-        </button>
-        <div className="expand__content">{info}</div>
+  toggleInfo = () => {
+    this.setState({
+      isFull: !this.state.isFull,
+    });
+  };
+
+  render() {
+    const info = this.state.isFull ? this.props.children : null;
+    const arrow = this.state.isFull ? (
+      <i className="fa fa-chevron-up" aria-hidden="true"></i>
+    ) : (
+      <i className="fa fa-chevron-down" aria-hidden="true"></i>
+    );
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{this.props.title}</span>
+          <button className="expand__toggle-btn" onClick={this.toggleInfo}>
+            {arrow}
+          </button>
+          <div className="expand__content">{info}</div>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
