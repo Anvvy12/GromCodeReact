6ccba7441df72f4a4a8900edb0347619753b67b9
Bogdan <youbody7@gmail.com>
2022-09-06 18:51:34 +0300

    s

--- lesson11/task2/src/App.jsx
+++ lesson11/task2/src/App.jsx
@@ -1,6 +1,41 @@
-import React from "react";
-import GoodButton from "./GoodButton";
+import React, { Component } from "react";
 
-export default () => {
-  return <GoodButton />;
-};
+import Dialog from "./Dialog";
+
+export default class extends Component {
+  state = {
+    isOpen: false,
+  };
+  hideDialog = () => {
+    this.setState({
+      isOpen: false,
+    });
+  };
+  showDialog = () => {
+    this.setState({
+      isOpen: true,
+    });
+  };
+
+  render() {
+    const elem =
+      "Use immutable array methods to work with data. It will help to avoid bugs";
+    return (
+      <div className="app">
+        <button className="btn" onClick={this.showDialog}>
+          Show dialog
+        </button>
+        <Dialog
+          isOpen={this.state.isOpen}
+          title={"Recommendation"}
+          onClouse={this.hideDialog}
+        >
+          <p>
+            Use immutable array methods to work with data. It will help to avoid
+            bugs
+          </p>
+        </Dialog>
+      </div>
+    );
+  }
+}
