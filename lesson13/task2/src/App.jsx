c761b6ca4a00988ab4f07cc328ebc10042853b1f
Bogdan <youbody7@gmail.com>
2022-09-08 17:31:06 +0300

    s

--- lesson13/task2/src/App.jsx
+++ lesson13/task2/src/App.jsx
@@ -1,6 +1,27 @@
 import React from "react";
-import GoodButton from "./GoodButton";
+import Products from "./Products";
+import Home from "./Home";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 
 export default () => {
-  return <GoodButton />;
+  return (
+    <div className="page">
+      <BrowserRouter>
+        <ul className="navigation">
+          <li className="navigation__item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navigation__item">
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </BrowserRouter>
+    </div>
+  );
 };
