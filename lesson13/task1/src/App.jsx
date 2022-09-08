d06fc4739b59dab9a1b500d2090a68017f13bcc0
Bogdan <youbody7@gmail.com>
2022-09-08 16:14:57 +0300

    s

--- lesson13/task1/src/App.jsx
+++ lesson13/task1/src/App.jsx
@@ -3,14 +3,27 @@ import Products from "./Products";
 import Contacts from "./Contacts";
 import Home from "./Home";
 import PageNotFound from "./PageNotFound";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 export default () => {
   return (
     <div className="page">
-      <Products />
-      <Contacts />
-      <Home />
-      <PageNotFound />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/products">
+            <Products />
+          </Route>
+          <Route path="/contacts">
+            <Contacts />
+          </Route>
+          <Route path="*">
+            <PageNotFound />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 };
