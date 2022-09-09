22c72e2e6ba9f37e636f4f61832e8b55849febaf
Bogdan <youbody7@gmail.com>
2022-09-09 18:06:59 +0300

    s

--- lesson14/task2/src/App.jsx
+++ lesson14/task2/src/App.jsx
@@ -1,29 +1,31 @@
 import React from "react";
-import Products from "./Products";
-import Contacts from "./Contacts";
-import Home from "./Home";
-import PageNotFound from "./PageNotFound";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import User from "./User";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 export default () => {
   return (
     <div className="page">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/products">
-            <Products />
-          </Route>
-          <Route path="/contacts">
-            <Contacts />
-          </Route>
-          <Route path="*">
-            <PageNotFound />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <Router>
+        <div className="page__content">
+          <h1>Users</h1>
+          <ul className="navigation">
+            <li className="navigation__item">
+              <Link to="/users/github">Github</Link>
+            </li>
+            <li className="navigation__item">
+              <Link to="/users/facebook">Facebook</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route path="/users/:userId">
+              <User />
+            </Route>
+            <Route path="/">
+              <span>Select a user please</span>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 };
