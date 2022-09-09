26d1c80854e35e1ff970917f9a7f1ac8965b4424
Bogdan <youbody7@gmail.com>
2022-09-09 18:38:34 +0300

    s

--- lesson14/task3/src/App.jsx
+++ lesson14/task3/src/App.jsx
@@ -1,29 +1,12 @@
 import React from "react";
-import Products from "./Products";
-import Contacts from "./Contacts";
-import Home from "./Home";
-import PageNotFound from "./PageNotFound";
+import Dimensions from "./Dimensions";
+
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 export default () => {
   return (
-    <div className="page">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/products">
-            <Products />
-          </Route>
-          <Route path="/contacts">
-            <Contacts />
-          </Route>
-          <Route path="*">
-            <PageNotFound />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+    <div class="dimensions">
+      <Dimensions />
     </div>
   );
 };
---
+++ lesson14/task3/src/Dimensions.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+const Dimensions = () => {
+  const [demensions, setDemensions] = useState({
+    width: null,
+    height: null,
+  });
+
+  useEffect(() => {
+    const { innerWidth, innerHeight } = window;
+    setDemensions({ width: innerWidth, height: innerHeight });
+
+    const handlerResize = (event) => {
+      const { innerWidth, innerHeight } = event.target;
+      setDemensions({ width: innerWidth, height: innerHeight });
+    };
+
+    window.addEventListener("resize", handlerResize);
+  }, []);
+
+  const { width, height } = demensions;
+  return <div className="dimensions">{`${width}px - ${height}px`}</div>;
+};
+
+export default Dimensions;
