75d6bba48b25fdd6a6308b4ba8177e2f2f153bad
Богдан <94869993+Anvvy12@users.noreply.github.com>
2022-10-26 11:32:52 +0300

    Delete todo-listFFF directory

--- todo-listFFF/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import App from "./App";
-import ReactDOM from "react-dom";
-import "./index.scss";
-
-const rootElem = document.querySelector("#root");
-
-ReactDOM.render(<App />, rootElem);
